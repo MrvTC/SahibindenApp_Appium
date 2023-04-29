10f4dd769f84e0eea513b5333d1482e1e2162362
Merve C <mervetascava@gmail.com>
2023-04-29 14:51:23 +0300

    update

--- src/test/java/runner/TestRunner.java
+++ src/test/java/runner/TestRunner.java
@@ -7,7 +7,7 @@ import org.testng.ITestResult;
 import org.testng.annotations.*;
 
 @CucumberOptions(
-        tags = "@CheckResult",
+        tags = "@CheckFilters",
         features = "src/test/resources/features",
         glue = {"stepdefinitions","utils"},
         plugin = {
